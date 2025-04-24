83abf7ee6da9a747c7050da689b740666db129af
Andrei Timofeev <andrei.timofeev@flightcontrol.space>
2025-04-24 16:08:36 +0300

    First bug fix

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -1,18 +1,26 @@
 #include <UartRS485.h>
 
-UartRS485 uart1(UART_NUM_1);
+// RX: GPIO16, TX: GPIO17 (на TX можно повесить пустой пин)
+UartRS485 uart2(Serial2, 16, 17);
 
 void handleTemp(int key, int value) {
   Serial.printf("[Temp] Key: %d, Value: %d\n", key, value);
 }
 
+void handleStatus(int key, int value) {
+  Serial.printf("[Status] Code: %d, Detail: %d\n", key, value);
+}
+
 void setup() {
   Serial.begin(115200);
-  uart1.begin(9600);
-  uart1.registerHandler(1, handleTemp);
-  uart1.inject("1|58|-20#");
+  uart2.begin(9600);
+  uart2.registerHandler(1, handleTemp);   // classifier 1
+  uart2.registerHandler(2, handleStatus); // classifier 2
+
+  uart2.inject("1|5800|-20#");
+  uart2.inject("2|100|1#");
 }
 
 void loop() {
-  uart1.dispatch();
+  uart2.dispatch();
 }
--- src/UartRS485.h
+++ src/UartRS485.h
@@ -1,49 +1,39 @@
 #pragma once
-#include <Arduino.h>
-#include "driver/uart.h"
 
-struct ParsedMessage {
-    int classifier;
-    int key;
-    int value;
-    bool valid;
-};
+#include <Arduino.h>
+#include <HardwareSerial.h>
 
 typedef void (*UartRS485_Handler)(int key, int value);
 
 class UartRS485 {
 public:
-    explicit UartRS485(uart_port_t port);
+    UartRS485(HardwareSerial& serial, int8_t rxPin = -1, int8_t txPin = -1);
 
-    void begin(uint32_t baud, int8_t rxPin = -1, int8_t txPin = -1);
-    bool available();
-    ParsedMessage read();
+    void begin(uint32_t baud);
     void dispatch();
-    void inject(const char* testMessage);
     bool registerHandler(int classifier, UartRS485_Handler handler);
+    void inject(const char* message);  // For test messages
 
 private:
-    static void IRAM_ATTR onUartISR(void* arg);
-    void handleByte(uint8_t c);
+    static constexpr size_t BUFFER_SIZE = 128;
+    static constexpr size_t MAX_HANDLERS = 10;
+    static constexpr char EOL = '#';
 
-    uart_port_t _port;
-
-    static const int BUF_SIZE = 256;
-    static const uint8_t CUSTOM_EOL = '#';
-    static const int MAX_HANDLERS = 10;
-
-    volatile uint8_t buffer1[BUF_SIZE];
-    volatile uint8_t buffer2[BUF_SIZE];
-
-    volatile uint8_t* active_buf;
-    volatile uint8_t* ready_buf;
-    volatile size_t active_pos;
-
-    struct HandlerEntry {
+    struct Handler {
         int classifier;
-        UartRS485_Handler handler;
+        UartRS485_Handler callback;
     };
 
-    HandlerEntry handlers[MAX_HANDLERS];
-    size_t handler_count;
+    HardwareSerial& _serial;
+    int8_t _rxPin;
+    int8_t _txPin;
+
+    char _buffer[BUFFER_SIZE];
+    size_t _pos = 0;
+    volatile bool _ready = false;
+
+    Handler _handlers[MAX_HANDLERS];
+    size_t _handlerCount = 0;
+
+    void parseBuffer();
 };
